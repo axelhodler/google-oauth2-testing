1a396bb2b91c7d275f403711cdef666535e96da5
xorrr <derxor@gmail.com>
2015-07-30 23:33:40 +0200

    implement minimal templating showcase

---
+++ src/main/java/co/hodler/Main.java
@@ -0,0 +1,19 @@
+package co.hodler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.Spark;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Spark.get("/hello", (request, response) -> {
+      Map<String, Object> attributes = new HashMap<>();
+      attributes.put("message", "Hello FreeMarker World");
+
+      return new ModelAndView(attributes, "index.ftl");
+  }, new FreeMarkerTemplateEngine());
+  }
+}
